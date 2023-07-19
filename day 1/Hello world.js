1399cc7be7bef3fe518164b9b2ff33a094843bc0
Samuel Oluwafemi Soaga <62216711+femisoaga@users.noreply.github.com>
2023-07-19 16:19:20 +0100

    Add files via upload

---
+++ day 1/Hello world.js
@@ -0,0 +1,9 @@
+// Leet code
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+
+var createHelloWorld = function() {
+    return function(...args) {
+       return "Hello World"
+    }
+};
---
+++ day 1/ReverseString.js
@@ -0,0 +1,10 @@
+// code wars
+// Complete the solution so that it reverses the string passed into it.
+
+
+function solution(str){
+    //   const splitString = str.split('')
+    //   const invertArray = splitString.reverse()
+    //   const newString= invertArray.join('')
+      return newString = str.split("").reverse().join("");
+    }
